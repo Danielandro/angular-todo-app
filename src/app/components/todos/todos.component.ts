4462bd5b80fbf7793dc7277908e0d93c3719b18c
Danielandro <mtme13@hotmail.com>
2020-01-05 20:06:10 +0000

    Add Todo model definition

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import Todo from "../../models/Todo";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  Todos: Todo[];
   ngOnInit() {
+    this.Todos = [
+      {
+        id: 1,
+        title: "Do laundry",
+        completed: false
+      },
+      {
+        id: 1,
+        title: "Buy cheese",
+        completed: true
+      },
+      {
+        id: 1,
+        title: "Fetch water",
+        completed: false
+      }
+    ];
   }
-
 }
